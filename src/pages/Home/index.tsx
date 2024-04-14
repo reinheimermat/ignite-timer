5251d095deedbaa1b3cab8a6df9372c123ce6525
reinheimermat <mreinheimer0@gmail.com>
2024-04-14 15:17:19 -0300

    feat: add suggestions list to task input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,13 +19,24 @@ export function Home() {
             type="text"
             id="task"
             placeholder="From a name to your task."
+            list="task-suggestions"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">for</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutes.</span>
