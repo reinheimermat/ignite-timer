1ba7885390e7641fda47acfb439ed627880ea637
reinheimermat <mreinheimer0@gmail.com>
2024-04-14 15:06:58 -0300

    feat: define the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
-    <>
-      <ThemeProvider theme={defaultTheme}>
-        <h1>Hello, world!</h1>
-
-        <GlobalStyle />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
