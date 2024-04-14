6b57293d68cf3dd60b7e2ca5e90e845e295ab2de
reinheimermat <mreinheimer0@gmail.com>
2024-04-14 15:34:10 -0300

    style: add styles to history page

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,53 @@
+import { HistoryContainer, HistoryList } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu jdjdjd</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 weeks ago</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 weeks ago</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 weeks ago</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 weeks ago</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 weeks ago</td>
+              <td>Finished</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
