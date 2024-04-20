0dbbc46416428d199f4d0db19f745a69efb54a04
reinheimermat <mreinheimer0@gmail.com>
2024-04-19 23:45:47 -0300

    refact: readjust function of resetting a cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -34,14 +34,19 @@ export function Home() {
     },
   })
 
-  const { /* reset */ handleSubmit, watch } = newCycleForm
+  const { reset, handleSubmit, watch } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
 
   const task = watch('task')
   const isSubmitDisabled = !task
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(createNewCycle)} action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormProvider {...newCycleForm}>
           <NewCycleForm />
         </FormProvider>
